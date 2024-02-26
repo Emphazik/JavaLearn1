1c61af0e6c9c2c466562707611305a5dad2e0fb6
Гуцуляк Владислав Витальевич <vladgutsulyak216@gmail.com>
2024-02-26 15:30:41 +0300

    Задача: Пользователь загадывает число в диапазоне от [1 до 100]. 2.Пользователь загадывает в уме, программа не хранит это число, а пытается его угадать. 3.Программа пытается его угадать (вместо обычного перебора постарайтесь 4.Использовать метод бинарного поиска, т.е деление отрезка на 2, пока число не будет найдено). 5.Программа может задавать пользователю вопросы: Число равно ...?, Число больше ...? и, в зависимости от ответа пользователя, принимать решения. 6.Вместо текстовых ответов ДА/НЕТ, используйте числа 0 - НЕТ и 1 - ДА

--- src/Ex3.java
+++ src/Ex3.java
@@ -1,2 +1,42 @@
+import java.util.Scanner;
+
 public class Ex3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int low = 1;
+        int high = 100;
+        boolean found = false;
+
+        System.out.println("Введите число от 1 до 100:");
+
+        int userNumber = scanner.nextInt();
+        if (userNumber < 1 || userNumber > 100) {
+            System.out.println("Пожалуйста, введите число в диапазоне от 1 до 100.");
+            return;
+        }
+
+        while (!found) {
+            int guess = (low + high) / 2;
+            System.out.println("Число равно " + guess + "? (Введите 1 если да, 0 если нет):");
+            int response = scanner.nextInt();
+
+            if (response == 1) {
+                System.out.println("Программа угадала ваше число!");
+                found = true;
+            } else if (response == 0) {
+                System.out.println("Число больше " + guess + "? (Введите 1 если да, 0 если нет):");
+                int answer = scanner.nextInt();
+                if (answer == 1) {
+                    low = guess + 1;
+                } else {
+                    high = guess - 1;
+                }
+            } else {
+                System.out.println("Пожалуйста, введите корректный ответ (1 или 0).");
+            }
+        }
+
+        scanner.close();
+    }
 }
