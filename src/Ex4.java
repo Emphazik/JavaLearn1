9fea9e94fb9fa73c78f981223ca500964c88c949
Гуцуляк Владислав Витальевич <vladgutsulyak216@gmail.com>
2024-02-26 15:33:48 +0300

    Задача: Осуществить ввод целого числа М. На промежутке от 1 до M найти все числа Армстронга. Натуральное число из n цифр называется числом Армстронга, если сумма его цифр, возведенных в n-ю степень, равна самому числу. Примеры: 153=13 + 53 + 33; 1634=14+64+34+44.

--- src/Ex4.java
+++ src/Ex4.java
@@ -1,2 +1,38 @@
+import java.util.Scanner;
+
 public class Ex4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите целое число M:");
+        int M = scanner.nextInt();
+
+        if (M <= 0) {
+            System.out.println("Пожалуйста, введите положительное целое число.");
+            return;
+        }
+
+        System.out.println("Числа Армстронга на промежутке от 1 до " + M + ":");
+        for (int i = 1; i <= M; i++) {
+            if (isArmstrong(i)) {
+                System.out.println(i);
+            }
+        }
+
+        scanner.close();
+    }
+
+    public static boolean isArmstrong(int number) {
+        int sum = 0;
+        int originalNumber = number;
+        int numDigits = String.valueOf(number).length();
+
+        while (number > 0) {
+            int digit = number % 10;
+            sum += Math.pow(digit, numDigits);
+            number /= 10;
+        }
+
+        return sum == originalNumber;
+    }
 }
