0edcbbb3cf14a8a06b5a9e20d675a24e5831f36c
Гуцуляк Владислав Витальевич <vladgutsulyak216@gmail.com>
2024-02-26 15:35:08 +0300

    Задача: Мастям игральных карт присвоены порядковые номера: 1 — пики, 2 — трефы, 3 — бубны, 4 — червы. Достоинству карт, старших десятки, присвоены номера: 11 — валет, 12 — дама, 13 — король, 14 — туз. Даны два целых числа: N — достоинство (6 <= N <= 14) и M — масть карты (1 <= M <= 4). Вывести название соответствующей карты вида «шестерка бубен», «дама червей», «туз треф» и т. п. Если пользователь введёт данные не соответствующие условию задачи - выдать сообщение об ошибке.

--- src/Ex5.java
+++ src/Ex5.java
@@ -1,2 +1,73 @@
+import java.util.Scanner;
 public class Ex5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите величину карты (от 6 до 14):");
+        int rank = scanner.nextInt();
+
+        if (rank < 6 || rank > 14) {
+            System.out.println("Ошибка: некорректное величина карты.");
+            return;
+        }
+        System.out.println("Введите масть карты (1 - пики, 2 - трефы, 3 - бубны, 4 - червы):");
+        int suit = scanner.nextInt();
+        if (suit < 1 || suit > 4) {
+            System.out.println("Ошибка: некорректная масть карты.");
+            return;
+        }
+        String rankName;
+        switch (rank) {
+            case 6:
+                rankName = "шестерка";
+                break;
+            case 7:
+                rankName = "семерка";
+                break;
+            case 8:
+                rankName = "восьмерка";
+                break;
+            case 9:
+                rankName = "девятка";
+                break;
+            case 10:
+                rankName = "десятка";
+                break;
+            case 11:
+                rankName = "валет";
+                break;
+            case 12:
+                rankName = "дама";
+                break;
+            case 13:
+                rankName = "король";
+                break;
+            case 14:
+                rankName = "туз";
+                break;
+            default:
+                rankName = "";
+        }
+
+        String suitName;
+        switch (suit) {
+            case 1:
+                suitName = "пики";
+                break;
+            case 2:
+                suitName = "трефы";
+                break;
+            case 3:
+                suitName = "бубны";
+                break;
+            case 4:
+                suitName = "черви";
+                break;
+            default:
+                suitName = "";
+        }
+
+        System.out.println(rankName + " " + suitName);
+
+        scanner.close();
+    }
 }
