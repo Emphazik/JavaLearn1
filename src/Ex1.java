73a401b8970835fe0f190c777d039f680b5e72de
Гуцуляк Владислав Витальевич <vladgutsulyak216@gmail.com>
2024-02-26 13:59:49 +0300

    Задача: Для введённого пользователем с клавиатуры целого положительного числа посчитайте сумму всех его цифр

--- src/Ex1.java
+++ src/Ex1.java
@@ -3,21 +3,23 @@ import java.util.Scanner;
 public class Ex1 {
 
         public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-
             System.out.println("Введите целое положительное число:");
-            int number = scanner.nextInt();
+            Scanner scanner = new Scanner(System.in);
+            try { //Происходит исключение
+                int number = scanner.nextInt();
+                if (number <= 0) {
+                    System.err.println("Введите положительное число.");
+                    return; // Завершаем программу
+                }
 
-            if (number <= 0) {
-                System.out.println("Введите положительное число.");
-                return; // Завершаем программу
+                int sum = sumOfDigits(number);
+                System.out.println("Сумма цифр числа: " + sum);
+            } catch (Exception e) { // Обработка исключения
+                System.err.println("Неизвестная ошибка! Не ломайте мой код!");
             }
-
-            int sum = sumOfDigits(number);
-            System.out.println("Сумма цифр числа: " + sum);
         }
 
-        // Метод для подсчета суммы цифр числа
+        // Метод для подсчета
         public static int sumOfDigits(int number) {
             int sum = 0;
             while (number != 0) {
@@ -27,5 +29,3 @@ public class Ex1 {
             return sum;
         }
     }
-
-
